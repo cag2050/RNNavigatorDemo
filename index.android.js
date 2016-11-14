18997e31687beab43f8ff1a7fe1e761906abd155
chenanguo <chenanguo@le.com>
2016-11-14 21:25:03 +0800

    m

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class RNNavigatorDemo extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!8888
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
---
+++ src/js/component/FirstPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class RNNavigatorDemo extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <Text>first page</Text>
+        )
+
+    }
+}
